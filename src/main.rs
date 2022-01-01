ed516661f9d75dbd586fa30c0c4104325fa59e53
Pär Bohrarper <par.bohrarper@gmail.com>
2022-01-01 22:29:45 +0100

    add playback and dpcm decoding

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 // -*- coding: utf-8 -*-
 
-use anyhow::{self, Context};
+use anyhow::{self};
 use clap::{App, Arg};
-use easy_repl::{command, repl::LoopStatus, validator, CommandStatus, Repl};
-use log::{debug, error, info, warn};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use easy_repl::{repl::LoopStatus, validator, CommandStatus, Repl};
 use std::cell::RefCell;
 use std::fs;
 use std::io::Read;
 use std::path::Path;
+use std::sync::{Arc, Barrier};
 use textplots::{Chart, Plot, Shape};
 
 #[derive(parse_display::Display, parse_display::FromStr, Debug, Copy, Clone)]
@@ -20,35 +21,50 @@ enum Representation {
     Custom,
 }
 
+#[derive(parse_display::Display, parse_display::FromStr, Debug, Copy, Clone)]
+#[display(style = "lowercase")]
+enum Compression {
+    DPCM0,
+    DPCM1,
+    DPCM2,
+    DPCM3,
+}
+
 #[derive(Copy, Clone, Debug)]
 struct Opts {
-    px: f32,
-    py: f32,
+    from: usize,
+    to: usize,
     step: usize,
     offset: u8,
     flip: u8,
     mirror: u8,
     sign: u8,
     representation: Representation,
+    compression: Compression,
 }
 
 impl Default for Opts {
     fn default() -> Opts {
         Opts {
-            px: 0.0,
-            py: 128.0,
+            from: 0,
+            to: 128,
             step: 2,
             offset: 0,
             flip: 0,
             mirror: 0,
-	    sign: 1,
+            sign: 1,
             representation: Representation::TwosComplement,
+            compression: Compression::DPCM0,
         }
     }
 }
 
 fn main() -> anyhow::Result<()> {
     env_logger::init();
+
+    let host = cpal::default_host();
+    let device = host.default_output_device().unwrap();
+    let config = device.default_output_config().unwrap();
 
     let matches = App::new("PCM Extract")
         .version("0.1")
@@ -73,12 +89,12 @@ fn main() -> anyhow::Result<()> {
         .get_matches();
 
     let filename = &matches.value_of("INPUT").unwrap();
-    error!("opening: {}", filename);
     let mut file = fs::File::open(Path::new(filename)).unwrap();
     let mut input = vec![];
     file.read_to_end(&mut input).unwrap();
 
     let opts = RefCell::new(Opts::default());
+    let play = RefCell::new(false);
     let mut out = vec![];
 
     let mut repl = Repl::builder()
@@ -153,6 +169,24 @@ fn main() -> anyhow::Result<()> {
             },
         )
         .add(
+            "compression",
+            easy_repl::Command {
+                description: "Set compression".into(),
+                args_info: vec![
+                    Compression::DPCM0.to_string(),
+                    Compression::DPCM1.to_string(),
+                    Compression::DPCM2.to_string(),
+                    Compression::DPCM3.to_string(),
+                ],
+                handler: Box::new(|args| {
+                    let validator = validator!(Compression);
+                    validator(args)?;
+                    opts.borrow_mut().compression = args[0].parse::<Compression>()?;
+                    Ok(CommandStatus::Done)
+                }),
+            },
+        )
+        .add(
             "step",
             easy_repl::Command {
                 description: "Set step".into(),
@@ -171,10 +205,21 @@ fn main() -> anyhow::Result<()> {
                 description: "Set plot range".into(),
                 args_info: vec![],
                 handler: Box::new(|args| {
-                    let validator = validator!(f32, f32);
+                    let validator = validator!(usize, usize);
                     validator(args)?;
-                    opts.borrow_mut().px = args[0].parse::<f32>()?;
-                    opts.borrow_mut().py = args[1].parse::<f32>()?;
+                    opts.borrow_mut().from = args[0].parse::<usize>()?;
+                    opts.borrow_mut().to = args[1].parse::<usize>()?;
+                    Ok(CommandStatus::Done)
+                }),
+            },
+        )
+        .add(
+            "play",
+            easy_repl::Command {
+                description: "Play range".into(),
+                args_info: vec![],
+                handler: Box::new(|_args| {
+                    *play.borrow_mut() = true;
                     Ok(CommandStatus::Done)
                 }),
             },
@@ -208,7 +253,7 @@ fn main() -> anyhow::Result<()> {
                         -(!d8 as i16)
                     }
                 }
-                Representation::TwosComplement => d8 as i16,
+                Representation::TwosComplement => (d8 as i8) as i16,
                 Representation::SignedMagnitude => {
                     if opt.sign == 0 {
                         let sign = d8 & 0x1;
@@ -228,7 +273,30 @@ fn main() -> anyhow::Result<()> {
                 }
                 Representation::ExcessK => (d8 as i16).overflowing_sub(opt.offset as i16).0,
             };
-            out.push(d.overflowing_mul(256).0);
+            match opt.compression {
+                Compression::DPCM0 => out.push(d.overflowing_mul(256).0),
+                Compression::DPCM1 => {
+                    let err = d;
+                    let n1 = if out.len() > 0 { out[out.len() - 1] } else { 0 };
+                    out.push(n1 + err);
+                }
+                Compression::DPCM2 => {
+                    let err = d;
+                    let n1 = if out.len() > 0 { out[out.len() - 1] } else { 0 };
+                    let n2 = if out.len() > 1 { out[out.len() - 2] } else { 0 };
+                    out.push((2 * n1 - n2) + err);
+                }
+                Compression::DPCM3 => {
+                    let err = d;
+                    let n1 = if out.len() > 0 { out[out.len() - 1] } else { 0 };
+                    let n2 = if out.len() > 1 { out[out.len() - 2] } else { 0 };
+                    let n3 = if out.len() > 2 { out[out.len() - 3] } else { 0 };
+                    out.push(3 * n1 - 3 * n2 + n3 + err);
+                }
+            }
+            if ix < 10 {
+                println!("d: {}, out: {}", d, out.last().unwrap());
+            }
             ix += opt.step;
             if ix >= input.len() {
                 break;
@@ -239,16 +307,59 @@ fn main() -> anyhow::Result<()> {
         for (i, x) in out.iter().enumerate() {
             plt.push((i as f32, *x as f32));
         }
-        Chart::new(300, 60, opt.px, opt.py)
+        Chart::new(300, 60, opt.from as f32, opt.to as f32)
             .lineplot(&Shape::Steps(&plt))
             .display();
         let mut plt2 = vec![];
         for (i, x) in input.iter().step_by(opt.step).enumerate() {
             plt2.push((i as f32, *x as f32));
         }
-        Chart::new(300, 60, opt.px, opt.py)
+        Chart::new(300, 60, opt.from as f32, opt.to as f32)
             .lineplot(&Shape::Steps(&plt2))
             .display();
+
+        if *play.borrow() {
+            *play.borrow_mut() = false;
+
+            let barrier = Arc::new(Barrier::new(2));
+
+            let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
+
+            let out_copy = out.clone();
+            let from = opt.from.min(out_copy.len());
+            let to = opt.to.min(out_copy.len());
+            let mut frames = 0;
+
+            let c = Arc::clone(&barrier);
+            let mut done = false;
+            let sc: cpal::StreamConfig = config.clone().into();
+            let stream = device.build_output_stream(
+                &sc,
+                move |data: &mut [i16], _: &cpal::OutputCallbackInfo| {
+                    for frame in data.chunks_mut(sc.channels as usize) {
+                        // up-sample
+                        let ix = from + frames / 4;
+                        if ix < to {
+                            let value = cpal::Sample::from::<i16>(&out_copy[ix]);
+                            for sample in frame.iter_mut() {
+                                *sample = value;
+                            }
+                        } else if !done {
+                            done = true;
+                            println!("no more data!");
+                            c.wait();
+                        }
+                        frames += 1;
+                    }
+                },
+                err_fn,
+            )?;
+            stream.play()?;
+            println!("wait..");
+            barrier.wait();
+            println!("done!");
+        }
+
         if let Ok(LoopStatus::Continue) = repl.next() {
         } else {
             break;
